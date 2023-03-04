576a33ec46c5c8eead0cde3e831ea835c7ed5c84
KarthikKallakuri2003 <112741363+KarthikKallakuri2003@users.noreply.github.com>
2023-03-03 22:34:36 -0600

    Delete form.js

--- src/form.js
+++
@@ -1,9 +0,0 @@
-const db = require("../db");
-
-
-const Form = db.model("Form", {
-    name: { type: String, required: true },
-    email:     { type: String, required: true }
- });
- 
- module.exports = Form;
